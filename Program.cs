204cdfc8f591076800b729426d2c91dbae732013
Nguyễn Thanh Viễn <nguyenthanhvienaf@gmail.com>
2025-03-02 09:12:49 +0700

    change port

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ app.UseExceptionHandler(errorApp =>
 	});
 });
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
 
@@ -61,5 +61,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseKestrel(options =>
+{
+	options.ListenAnyIP(int.Parse(port));
+});
 app.Run();
 
